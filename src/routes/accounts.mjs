5c146b1b53ea0a865e646e8a3e316935ecc4e0f4
Yuragranovsky <yuragranovsky@gmail.com>
2024-12-05 21:42:39 +0200

    CW #74 introducing Basic authentication

--- src/routes/accounts.mjs
+++ src/routes/accounts.mjs
@@ -1,18 +1,24 @@
 import express from 'express';
 export const accounts_route = express.Router();
 import asyncHandler from 'express-async-handler';
-import AccountsService from '../service/AccountsService.mjs';
 import { ACCOUNTS_ACCOUNT, DELETE_GET_ACCOUNT } from '../config/pathes.mjs';
-const accountsService = new AccountsService(process.env.MONGO_URI, process.env.DB_NAME);
+import { accountsService } from '../app.mjs';
+import { getError } from '../errors/error.mjs';
 accounts_route.post(ACCOUNTS_ACCOUNT, asyncHandler(async (req, res) => {
     const account = await accountsService.insertAccount(req.body);
     res.status(201).json(account);
 }));
 accounts_route.put(ACCOUNTS_ACCOUNT, asyncHandler(async (req, res) => {
+    if(req.user != req.body.username) {
+        throw getError(403, "");
+    }
    const account = await accountsService.updatePassword(req.body);
    res.status(200).json(account);
 }));
 accounts_route.get(DELETE_GET_ACCOUNT, asyncHandler(async (req, res) => {
+    if(req.user != req.body.username && req.role != "ADMIN") {
+        throw getError(403, "");
+    }
     const account = await accountsService.getAccount(req.params.username);
    res.status(200).json(account);
 }))
